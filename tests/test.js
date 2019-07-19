8de59142c20c70cf2553bd72866a2606b465eac4
Evan Hsu <evanhsu@gmail.com>
2019-07-19 08:10:12 +0000

    Updated unit test

--- tests/test.js
+++ tests/test.js
@@ -2,15 +2,13 @@
 
 const supertest = require('supertest'); 
 const test = require('unit.js');
-const app = require('../app.js');
+const app = require('../server.js');
 
 const request = supertest(app);
 
 describe('Tests app', function() {
   it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
-      test.string(result.text).contains('Congratulations');
-      test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
+    request.get('/api/todos').expect(200).end(function(err, result) {
       done(err);
     });
   });
